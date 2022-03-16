4895f8094ffdd1194e17a9c5bffbdcf07e0e6d5e
Jayesh Gajanan Chandan <jayeshchandan269@gmail.com>
2022-03-16 17:01:29 +0530

    User System

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib import auth
+from django.contrib import auth, messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+
+
+def register(request):
+    form = UserRegisterForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('home')
+    return render(request, 'accounts/register.html', {'form': form})
 
 
 # Create your views here.
